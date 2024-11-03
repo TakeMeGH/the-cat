2dbaae693d390a3043cc088e913aebe067626af0
Abdul Rafi <i_abdul@agate.id>
2024-11-03 08:38:59 +0700

    add main menu ui managment

--- Assets/Scripts/SceneManager/MainMenuManager.cs
+++ Assets/Scripts/SceneManager/MainMenuManager.cs
@@ -7,10 +7,48 @@ namespace TC
     public class MainMenuManager : MonoBehaviour
     {
         [SerializeField] InputReader _inputReader;
+        [SerializeField] GameObject _settingsMenu;
+        [SerializeField] GameObject _creditsMenu;
+        [SerializeField] GameObject _tutorialMenu;
+
+        GameObject _selectedMenu = null;
 
         void Start()
         {
             _inputReader.EnableUIInput();
         }
+
+        public void OpenSettingsMenu()
+        {
+            if (_selectedMenu == null)
+            {
+                _selectedMenu = _settingsMenu;
+                _selectedMenu.SetActive(true);
+            }
+        }
+
+        public void OpenCreditsMenu()
+        {
+            _selectedMenu = _creditsMenu;
+            _selectedMenu.SetActive(true);
+        }
+
+        public void OpenTutorialMenu()
+        {
+            _selectedMenu = _tutorialMenu;
+            _selectedMenu.SetActive(true);
+
+        }
+
+
+        public void CloseMenu()
+        {
+            if (_selectedMenu != null)
+            {
+                _selectedMenu.SetActive(false);
+                _selectedMenu = null;
+            }
+        }
+
     }
 }
